6fbc7585eb5f8b675a1f633213bfccbe693e3246
WindAsMe <542636539@qq.com>
2018-08-01 03:34:56 +0800

    NUS Smart Home Android

--- app/src/main/java/com/example/zhongrui/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/zhongrui/myapplication/Activity/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.zhongrui.myapplication.Activity;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.zhongrui.myapplication.R;
+import com.alibaba.fastjson.*;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,5 +76,54 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        new Thread(new Runnable() {
+            OkHttpClient client = new OkHttpClient();
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(5000);
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Request request = new Request
+                                            .Builder()
+                                            .url("http://13.67.109.181:8080/api/get")
+                                            .build();
+                                    Log.v("url {} ", request.toString());
+                                    Response response = client.newCall(request).execute();
+                                    String s = response.body().string();
+                                    JSONObject object = JSONObject.parseObject(s);
+                                    JSONObject meta = object.getJSONObject("meta");
+                                    if (meta.get("code").equals(0)) {
+                                        Handler handler = new Handler(Looper.getMainLooper());
+                                        handler.post(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Toast.makeText(MainActivity.this,
+                                                        "有陌生人闯入了您的家中！", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                        PackageManager packageManager = getPackageManager();
+                                        Intent intent = packageManager.getLaunchIntentForPackage("com.realvnc.viewer.android");
+                                        startActivity(intent);
+                                    }
+                                } catch (Exception e) {
+                                    Log.v("Exception ", e.getMessage());
+                                }
+                            }
+                        }).start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
+
+
+
+
 }
