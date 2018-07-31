55c660d8c7d30b34a2acd2d4b9f44d7ff6ab8dc5
WindAsMe <542636539@qq.com>
2018-07-31 16:13:17 +0800

    NUS Smart Home Android

--- app/src/main/java/com/example/zhongrui/myapplication/Activity/AllActivity.java
+++ app/src/main/java/com/example/zhongrui/myapplication/Activity/AllActivity.java
@@ -1,13 +1,17 @@
 package com.example.zhongrui.myapplication.Activity;
 
 import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.zhongrui.myapplication.R;
@@ -17,13 +21,15 @@ import com.example.zhongrui.myapplication.models.commomModels.AllDataModel;
 import com.example.zhongrui.myapplication.models.commomModels.MetaModel;
 import com.example.zhongrui.myapplication.util.HttpUtil;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AllActivity extends AppCompatActivity {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-    private HttpUtil httpUtil = new HttpUtil();
-    private String time = "2018年12月31日 15:21:01";
+public class AllActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -35,43 +41,128 @@ public class AllActivity extends AppCompatActivity {
         final Button button2 = findViewById(R.id.back);
         final TextView textView = findViewById(R.id.all);
 
+        // Waiting Adjusting...
+        final String url = "http://13.67.109.181:8080/all";
         try {
-            String json = "{\n" +
-                    "    \"meta\": {\n" +
-                    "        \"code\": 0,\n" +
-                    "        \"errorMsg\": null\n" +
-                    "    },\n" +
-                    "    \"PressDataModel\": {\n" +
-                    "        \"temp\": \"30.0\",\n" +
-                    "        \"humid\": \"88.0\",\n" +
-                    "        \"time\": \"2018-07-29 10:53:20\",\n" +
-                    "        \"press\": \"0.99\"\n" +
-                    "    }\n" +
-                    "}";
+            final OkHttpClient client = new OkHttpClient();
+            new Thread(new Runnable() {
+                Handler handler = new Handler(Looper.getMainLooper());
+                @Override
+                public void run() {
+                    try {
+
+                        // Send the Request
+                        Request request = new Request
+                                .Builder()
+                                .url(url)
+                                .build();
+                        Response response = client.newCall(request).execute();
+
+                        Log.v("invoke url{}", url);
+                        String s = response.body().string();
+                        JSONObject object = JSONObject.parseObject(s);
+                        JSONObject meta = object.getJSONObject("meta");
+                        // Success
+                        if (Integer.valueOf(0).equals(meta.get("code"))) {
+                            final JSONObject data = object.getJSONObject("data");
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textView.setText("温度: " + data.get("temp") +
+                                            "°C                                " +
+                                            "湿度: " + data.get("humid") +
+                                            "Pa                                " +
+                                            "气压: " + data.get("press") +
+                                            "Pa                                " +
+                                            "时间: " + data.get("time"));
+                                }
+                            });
+                        } else {
+                            Log.v("invoke Toast{}", meta.toString());
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(AllActivity.this,
+                                            "请求失败, 请稍后重试！", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    } catch (IOException e) {
+                        Log.v("Exception", e.getMessage());
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(AllActivity.this,
+                                        "网络异常,请检查网络！", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }).start();
 
 
-            JSONObject meta = JSONObject.parseObject(json).getJSONObject("meta");
-            if (Integer.valueOf(0).equals(meta.get("code"))) {
-                JSONObject data = JSONObject.parseObject(json).getJSONObject("PressDataModel");
-                textView.setText("温度: " + data.get("temp") + "°C                                "
-                        + "湿度: " + data.get("humid")  + "%                                "
-                        + "气压: " + data.get("press")  + "Pa                                "
-                        + "时间: " + data.get("time") );
-            }
-
+            // TODO: okHttp GET the Response
             button1.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View view) {
-                    textView.setText("温度: " + (float)(Math.random() * 35) + "°C                                "
-                            + "湿度: " + (float)(Math.random() * 100) + "%                                "
-                            + "气压: " + (float)(Math.random() * 2) + "Pa                                "
-                            + "时间: " + time);
+                public void onClick(final View view) {
+                    new Thread(new Runnable() {
+                        // subprocess pop Toast needed
+                        Handler handler = new Handler(Looper.getMainLooper());
+                        @Override
+                        public void run() {
+                            try {
+                                // Send the Request
+                                // url is adjustment needed
+                                Request request = new Request.Builder().url(url).build();
+                                Log.v("invoke url{}", url);
+                                Response response = client.newCall(request).execute();
+                                String s = response.body().string();
+
+                                JSONObject object = JSONObject.parseObject(s);
+                                JSONObject meta = object.getJSONObject("meta");
+
+                                // Success
+                                if (Integer.valueOf(0).equals(meta.get("code"))) {
+                                    final JSONObject data = object.getJSONObject("data");
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            textView.setText("温度: " + data.get("temp") +
+                                                    "°C                                " +
+                                                    "湿度: " + data.get("humid") +
+                                                    "Pa                                " +
+                                                    "气压: " + data.get("press") +
+                                                    "Pa                                " +
+                                                    "时间: " + data.get("time"));
+                                        }
+                                    });
+                                } else {
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(view.getContext(),
+                                                    "请求失败, 请稍后重试！", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            } catch (IOException e) {
+                                Log.v("Exception", e.getMessage());
+                                handler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(view.getContext(),
+                                                "网络异常,请检查网络！", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }
+                    }).start();
                 }
             });
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
